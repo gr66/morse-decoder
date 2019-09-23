98482aa207d2b2f19b5e69730601c5061cd69df6
Gregory Kravchenko <2crazymail@gmail.com>
2019-09-23 22:36:05 +0300

    Solution

--- src/index.js
+++ src/index.js
@@ -39,6 +39,30 @@ const MORSE_TABLE = {
 
 function decode(expr) {
     // write your solution here
+    let res = "";
+    for (let i = 0; i < expr.length; i+=10) {
+        let  symbol = expr.slice(i, i + 10)
+        res += symbol == "**********" ? " " : MORSE_TABLE[getMorseSymbol(symbol)];
+    }
+    return res;
+}
+
+let getMorseSymbol = function(symbol) {
+    let res = "";
+    for(let i=0; i< symbol.length; i += 2) {
+        switch(symbol.slice(i, i + 2)) {
+            case "00":
+                res += "";
+                break;
+            case "10":
+                res += ".";
+                break;
+            case "11":
+                res += "-";
+                break;
+        }
+    }
+    return res;
 }
 
 module.exports = {
